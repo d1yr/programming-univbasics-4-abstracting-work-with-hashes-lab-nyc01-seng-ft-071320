0d75773ceacda23a438876b9663535cbedd86ea3
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-10 04:16:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,9 @@
 def my_hash_creator(key, value)
-   hash = { key => value }
+   hash[key => value]
 end
 
 def read_from_hash(hash, key)
   hash[key]
-  
 end
 
 def update_counting_hash(hash, key)
